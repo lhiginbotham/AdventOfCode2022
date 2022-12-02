9c68c1081e82e7119a10eeeb99dfa503f79143de
Logan Higinbotham <lgh8@pitt.edu>
2022-12-02 00:09:08 -0500

    Day 2: rock-paper-scissors score calculator

---
+++ 2/2.py
@@ -0,0 +1,40 @@
+inp = []
+with open('input.txt') as f:
+    inp = f.readlines()
+    for i in range(0, len(inp)):
+        inp[i] = inp[i].rstrip()
+
+round_scores = []
+r = 1
+p = 2
+s = 3
+
+l = 0
+d = 3
+w = 6
+
+for match in inp:
+    opp, me = match.split()
+    if opp == 'A':
+        if me == 'X':
+            round_scores.append(s + l)
+        elif me == 'Y':
+            round_scores.append(r + d)
+        elif me == 'Z':
+            round_scores.append(p + w)
+    elif opp == 'B':
+        if me == 'X':
+            round_scores.append(r + l)
+        elif me == 'Y':
+            round_scores.append(p + d)
+        elif me == 'Z':
+            round_scores.append(s + w)
+    elif opp == 'C':
+        if me == 'X':
+            round_scores.append(p + l)
+        elif me == 'Y':
+            round_scores.append(s + d)
+        elif me == 'Z':
+            round_scores.append(r + w)
+
+print(sum(round_scores))
